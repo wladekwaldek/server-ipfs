d26928b328882266692b67659c6ae848e752539f
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-01-24 13:10:10 +0100

    seconde

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hi");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+async function start() {
+  try {
+    app.listen(5000, () => {
+      console.log("Server has been started on port" + 5000);
+    });
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+}
+start();
