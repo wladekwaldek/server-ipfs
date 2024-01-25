9ff26816ea1687f9e63c36aec3932c5016270983
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-01-25 21:07:08 +0100

    fdbjsio

--- index.js
+++ index.js
@@ -7,36 +7,6 @@ const ipfs = ipfsClient();
 
 const app = express();
 
-if (NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    ipfs
-      .cat(req.url)
-      .then((response) => {
-        console.log("Содержимое файла:", response.toString());
-      })
-      .catch((error) => {
-        // Обработка ошибки
-        if (error.message.includes("404")) {
-          res.redirect("/");
-        } else {
-          console.error("Произошла ошибка:", error);
-        }
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-      });
-  });
-}
-
-app.use((req, res, next) => {
-  res.status(404).send("Извините, запрашиваемая страница не найдена.");
-});
-
-// Обработчик ошибок
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Что-то пошло не так!");
-});
-
 async function start() {
   try {
     app.listen(5000, () => {
