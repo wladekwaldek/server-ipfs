613bd65cb9e97aa8f69ca91a4a4a55707758e112
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-01-25 21:50:59 +0100

    iedrfguysjdi

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
-const ipfsClient = require("ipfs-http-client");
-
-const ipfs = ipfsClient();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 async function start() {
   try {
