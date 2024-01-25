05741a9258c409690adfbda8424986a292343f35
wladekwaldek <60227357+wladekwaldek@users.noreply.github.com>
2024-01-25 13:43:52 +0100

    prod3

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const http = require("http");
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 async function start() {
   try {
